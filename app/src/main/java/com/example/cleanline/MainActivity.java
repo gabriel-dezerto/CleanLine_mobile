88b913e83d3493e5b329c0d227b20bb5dffaa61a
gabriel-dezerto <dezertogabriel@gmail.com>
2026-04-10 15:33:55 -0300

    Adicionando validações simples

--- app/src/main/java/com/example/cleanline/MainActivity.java
+++ app/src/main/java/com/example/cleanline/MainActivity.java
@@ -11,9 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogin;
+    TextInputEditText etEmail, etSenha;
+    TextInputLayout tilEmail, tilSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,36 @@ public class MainActivity extends AppCompatActivity {
 
         btnLogin = findViewById(R.id.btnLogin);
 
+        etEmail = findViewById(R.id.etEmail);
+        etSenha = findViewById(R.id.etSenha);
+
+        tilEmail = findViewById(R.id.tilEmail);
+        tilSenha = findViewById(R.id.tilSenha);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = etEmail.getText().toString().trim();
+                String senha = etSenha.getText().toString().trim();
+
+                tilEmail.setError(null);
+                tilSenha.setError(null);
+
+                if(email.isEmpty()){
+                    tilEmail.setError("Informe o email");
+                    return;
+                }
+
+                if(!email.contains("@")){
+                    tilEmail.setError("Email inválido");
+                    return;
+                }
+
+                if(senha.isEmpty()){
+                    tilSenha.setError("Informe a senha");
+                    return;
+                }
+
                 Intent intent = new Intent(MainActivity.this, HomeActivity.class);
 
                 startActivity(intent);
