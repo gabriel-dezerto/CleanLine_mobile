839b6e58489df428a89e9a77160086295d6d7d04
gabriel-dezerto <dezertogabriel@gmail.com>
2026-04-17 17:12:49 -0300

    Fazendo página de rotas

---
+++ app/src/main/java/com/example/cleanline/model/Rotas.java
@@ -0,0 +1,13 @@
+package com.example.cleanline.model;
+
+public class Rotas {
+    private String setor;
+
+    public Rotas(String setor) {
+        this.setor = setor;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+}
