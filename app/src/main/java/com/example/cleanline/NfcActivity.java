7a859eeea08e8a1772aae4c87c4507b2bdfc10d9
gabriel-dezerto <dezertogabriel@gmail.com>
2026-05-12 15:27:31 -0300

    Scanner NFC funcionando

--- app/src/main/java/com/example/cleanline/NfcActivity.java
+++ app/src/main/java/com/example/cleanline/NfcActivity.java
@@ -1,11 +1,18 @@
 package com.example.cleanline;
 
+import android.app.ComponentCaller;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,6 +21,9 @@ import androidx.core.view.WindowInsetsCompat;
 public class NfcActivity extends AppCompatActivity {
 
     Button btnCancelar;
+
+    private NfcAdapter nfcAdapter;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +41,52 @@ public class NfcActivity extends AppCompatActivity {
         btnCancelar.setOnClickListener(v -> {
             finish();
         });
+
+        // Inicializa o adaptador NFC
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter == null){
+            Toast.makeText(this, "NFC não disponível neste dispositivo", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Prepara a Intent para que o sistema saiba que este app quer processar a tag
+        Intent intent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Ativa o modo de prioridade
+        if (nfcAdapter != null){
+            IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+            IntentFilter[] filters = new IntentFilter[]{tagDetected};
+
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Desativa a prioridade ao sair da tela
+        if (nfcAdapter != null){
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction()) || NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
+
+            // Verifica se a intenção que chegou é um NFC lido
+            Intent mudarTela = new Intent(this, FormsActivity.class);
+            startActivity(mudarTela);
+        }
     }
 }
