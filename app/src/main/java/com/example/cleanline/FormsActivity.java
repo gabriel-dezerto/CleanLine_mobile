c705de6aba14ab40e0792e908e1463808745c550
gabriel-dezerto <dezertogabriel@gmail.com>
2026-05-12 16:22:56 -0300

    .

--- app/src/main/java/com/example/cleanline/FormsActivity.java
+++ app/src/main/java/com/example/cleanline/FormsActivity.java
@@ -1,6 +1,8 @@
 package com.example.cleanline;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FormsActivity extends AppCompatActivity {
+
+    RadioButton rbP1Exc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,18 @@ public class FormsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        rbP1Exc = findViewById(R.id.rbP1Exc);
+
+        RadioGroup radioGroup = findViewById(R.id.rgPergunta1);
+
+        if (radioGroup.getCheckedRadioButtonId() != -1) {
+            // Um botão foi selecionado
+            RadioButton radioButtonSelecionado = findViewById(radioGroup.getCheckedRadioButtonId());
+            String textoSelecionado = radioButtonSelecionado.getText().toString();
+        } else {
+            // Nenhum botão foi selecionado
+        }
+
     }
 }
